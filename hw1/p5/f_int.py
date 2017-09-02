63bd0760cec8d40dd7a200dd53cf321344ea6d6b
Michael Harrington <michaeltoddharrington@gmail.com>
2017-09-01 19:42:33 -0500

    Problem 5 complete

---
+++ hw1/p5/f_int.py
@@ -0,0 +1,29 @@
+"""
+This file provides a non recursive alternative to a given function
+"""
+
+from math import ceil
+
+def f(n):
+    if n > 100:
+        return n-10
+    else:
+        return f(f(n+11))
+
+def new_f(n):
+    if n > 100:
+        return n - 10
+    else:
+        offset = ceil(n) - n
+        return 91 - offset
+
+if __name__ == '__main__':
+    test_input = [
+        1, -6, 200, 27,
+        0, -1, 102, 106, 10010, -100, -10232, -23, -230, 0.5, 99.5, 1.2
+    ]
+
+    for t_input in test_input:
+        old_out = f(t_input)
+        new_out = new_f(t_input)
+        print('n =', t_input, '  \t', 'f(n) =', old_out, '\t', 'new_f(n) =', new_out)

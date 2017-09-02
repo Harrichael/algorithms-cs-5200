0e672bd279db70548b47313b8c2bf8409e4ac712
Michael Harrington <michaeltoddharrington@gmail.com>
2017-09-01 23:26:45 -0500

    Problem 9 updated

--- hw1/p9/anagram.py
+++ hw1/p9/anagram.py
@@ -2,6 +2,24 @@
 This file creates two anagram generators and compares them
 """
 
+import time
+import string
+
+class Timer(object):
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+        self.elapsed_time = None
+
+    def __enter__(self):
+        self.start_time = time.time()
+        self.end_time = None
+        self.elapsed_time = None
+
+    def __exit__(self, *args, **kwargs):
+        self.end_time = time.time()
+        self.elapsed_time = self.end_time - self.start_time
+
 def anagram1(string):
     """
     This anagram func uses multiple recursive calls
@@ -37,6 +55,34 @@ def anagram2(string):
         return anagrams            
 
 if __name__ == '__main__':
-    anagram1('hello wor')
-    anagram2('hello wor')
+    test_funcs = [anagram1, anagram2]
+    test_cases = list(range(11))
+    min_repeats = 3
+    max_repeats = 100
+    min_time = 5
+    timer = Timer()
+    data = {}
+    for func in test_funcs:
+        data[func.__name__] = dict([(n, []) for n in test_cases])
+
+    for string_len in test_cases:
+        test_string = string.ascii_uppercase[:string_len]
+        for func in test_funcs:
+            test_repeats = 0
+            test_case_time = 0
+        
+            while (test_repeats < max_repeats) and (test_repeats < min_repeats or test_case_time < min_time):
+                with timer:
+                    func(test_string)
+
+                data[func.__name__][string_len].append(timer.elapsed_time)
+
+                test_repeats += 1
+                test_case_time += timer.elapsed_time
+
+    print('func,' + ','.join(map(str, test_cases)))
+    for func in test_funcs:
+        f_data = data[func.__name__]
+        best_times = [min(f_data[tc]) for tc in test_cases]
+        print(func.__name__ + ',' + ','.join(map(str, best_times)))
 

1611ff248f98fdbe6b72e8e5e67e64a289ec1a1b
Michael Harrington <michaeltoddharrington@gmail.com>
2017-09-11 03:09:58 -0500

    problem 3 complete

--- hw2/p3/estimator.py
+++ hw2/p3/estimator.py
@@ -12,8 +12,11 @@ def estimate_1(n):
 def estimate_2(n):
     return (n*(n+1))/2
 
+def estimate_3(n):
+    return (-1)**(n-1) * estimate_2(n)
+
 if __name__ == '__main__':
-    estimator = estimate_2
+    estimator = estimate_3
     for n in range(20):
         val = f(n)
         est = estimator(n)

dafbbee6b2cd5b0550ceb1e8df2bd25d9a8c463d
Michael Harrington <michaeltoddharrington@gmail.com>
2017-09-11 05:26:05 -0500

    problem 6 complete

---
+++ hw2/p6/halt_test.py
@@ -0,0 +1,17 @@
+
+def depth(n):
+    if n < 2:
+        return 1
+    if n % 2 == 1:
+        return 1 + depth(3*n + 1)
+    else:
+        return 1 + depth(n/2)
+
+if __name__ == '__main__':
+    low_bound = 1
+    hi_bound = 100
+    for val in range(low_bound, hi_bound + 1):
+        depth(val)
+
+    print('Depth function did not hault for numbers {} to {}'.format(low_bound, hi_bound))
+

b9282f6d6e0f7bd42af44d613e93bc6ba75abd4f
Michael Harrington <michaeltoddharrington@gmail.com>
2017-09-26 22:48:34 -0500

    Update to old problem

--- hw3/p1/rec_int.py
+++ hw3/p1/rec_int.py
@@ -1,5 +1,5 @@
 
-c = 17
+c = 14
 
 def f(n):
     if n > 100:
@@ -7,7 +7,18 @@ def f(n):
     else:
         return f(f(n + c))
 
+def simpl_f(n):
+    if n > 100:
+        return n - 10
+    elif c <= 10:
+        while True:
+            pass
+    else:
+        period = c - 10
+        diff = 100 - n
+        return 90 + period - diff % period
+
 if __name__ == '__main__':
-    for n in range(100 - 2*c, 100 + 2*c):
-        print(n, f(n))
+    for n in range(100 - 2*c, 100):
+        print(n, f(n), simpl_f(n))
 
